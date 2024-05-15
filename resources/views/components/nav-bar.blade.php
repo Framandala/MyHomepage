a4cf4418697d61cddd3e8e17bd9414aa1fd39ebd
Framandala <aq506997@gmail.com>
2024-05-15 20:11:15 +0200

    navbar finita e responsive

---
+++ resources/views/components/nav-bar.blade.php
@@ -0,0 +1,13 @@
+<div class="shadow">
+    <div class="navbar">
+        <div class="navbar-left">
+            <button>Progetti</button>
+        </div>
+        <div class="navbar-center ">
+            <img class="logo" src="./img/Logo.png" alt="Logo">
+        </div>
+        <div class="navbar-right">
+            <button>Contatti</button>
+        </div>
+    </div>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+
+    <x-nav-bar></x-nav-bar>
+
+    <ul class="nav nav-underline justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Progetti</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contatti</a>
+        </li>
+      </ul>
+
+</x-layout>
